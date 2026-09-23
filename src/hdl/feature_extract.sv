4fbb6996b7e838f9e67ec0d3b3ef02a59b64de0d
Adam Welsh <adamwelsh.college@gmail.com>
2026-09-23 12:17:52 -0600

    work on bram dump sm

--- src/hdl/feature_extract.sv
+++ src/hdl/feature_extract.sv
@@ -26,16 +26,27 @@ module feature_extract #(
     output logic o_dump_complete
 );
 
-// feature tables (32 bits wide)
+// feature tables (32 bits wide except for y)
 logic [31:0] area_ram [0:NUM_LABELS-1];
 logic [31:0] sum_x_ram [0:NUM_LABELS-1];
-logic [31:0] sum_y_ram [0:NUM_LABELS-1];
+logic [19:0] sum_y_ram [0:NUM_LABELS-1];
 
 // zeros table
 logic zeros_ram [0:NUM_LABELS-1];
 
 logic [LABEL_SIZE-1:0] addra, addrb;
 logic we_a_ft, we_b_zt;
+
+typedef enum logic[3:0] {
+    S_accum         = 3'd0,
+    S_dump_read     = 3'd1,
+    S_dump_w1       = 3'd2,
+    S_dump_w2       = 3'd3,
+    S_dump_head     = 3'd4,
+    S_done          = 3'd5
+} state_t;
+
+state_t state, next_state;
 
 assign we_a_ft = i_valid_label;
 assign we_b_zt = i_valid_coll;
@@ -52,23 +63,30 @@ always_comb begin
 end
 
 // read port A
-logic [31:0] read_area_a, read_x_a, read_y_a;
+logic [31:0] read_area_a, read_x_a;
+logic [19:0] read_y_a;
 assign read_area_a  = area_ram[addra] & {32{zeros_ram[addra]}};
 assign read_x_a     = sum_x_ram[addra] & {32{zeros_ram[addra]}};
-assign read_y_a     = sum_y_ram[addra] & {32{zeros_ram[addra]}};
+assign read_y_a     = sum_y_ram[addra] & {20{zeros_ram[addra]}};
 
 // read port b
-logic [31:0] read_area_b, read_x_b, read_y_b;
+logic [31:0] read_area_b, read_x_b;
+logic [19:0] read_y_b;
 assign read_area_b  = area_ram[addrb] & {32{zeros_ram[addrb]}};
 assign read_x_b     = sum_x_ram[addrb] & {32{zeros_ram[addrb]}};
-assign read_y_b     = sum_y_ram[addrb] & {32{zeros_ram[addrb]}};
+assign read_y_b     = sum_y_ram[addrb] & {20{zeros_ram[addrb]}};
 
 
 // accumulator signals and math
-logic [31:0] new_area, new_x, new_y;
+logic [31:0] new_area, new_x;
+logic [19:0] new_y;
 
 always_comb begin
-    if (i_valid_coll) begin
+    if (state != S_accum) begin
+        addra = read_ptr;
+        addrb = '0;
+    end 
+    else if (i_valid_coll) begin
         new_area    = read_area_a + read_area_b + 1'b1;
         new_x       = read_x_a + read_x_b + i_x_coord;
         new_y       = read_y_a + read_y_b + i_y_coord;
@@ -97,6 +115,79 @@ always_ff @(posedge i_clk) begin
             zeros_ram[addrb]    <= 1'b0;
         end
     end
+end
+
+
+
+
+
+// state machine stuff
+
+logic [LABEL_SIZE-1:0] read_ptr;
+logic [7:0] valid_centroid_count;
+logic [1:0] frame_slot;
+logic [31:0] bram_base_addr;
+logic [31:0] current_bram_addr;
+
+always_ff @(posedge clk) begin
+    if (i_rst) begin
+        state <= S_accum;
+        read_ptr <= 1;
+        valid_centroid_count <= '0;
+        frame_slot <= 0;
+    end else begin
+        state <= next_state;
+    end
+end
+
+always_comb begin
+    next_state  = state;
+    o_bram_we   = 4'b0000;
+    o_bram_en   = 1'b0;
+    o_dump_complete = 1'b0;
+
+    case(state) 
+        S_accum: begin
+            if (i_frame_done) begin
+                next_state = S_dump_read;
+            end
+        end
+        S_dump_read: begin
+            if(read_ptr == NUM_LABELS) begin
+                next_state = S_dump_head;
+            end
+            else if (read_area_a > 0) begin
+                next_state = S_dump_w1;
+            end
+            else begin
+                next_state = S_dump_read;
+            end
+        end
+        S_dump_w1: begin
+            o_bram_en = 1'b1;
+            o_bram_we = 4'b1111;
+            o_bram_addr = current_bram_addr;
+            o_bram_wdata = read_x_a;
+            next_state = S_dump_w2;
+        end
+        S_dump_w2: begin
+            o_bram_en = 1'b1;
+            o_bram_we = 4'b1111;
+            o_bram_addr = current_bram_addr;
+            o_bram_wdata = {4'b0, read_y_a, read_area_a, 1'b1};
+            next_state = S_dump_read;
+        end
+        S_dump_head: begin
+
+
+        end
+        S_done: begin
+
+        end
+        default: begin
+
+        end
+    endcase
 end
 
 endmodule
